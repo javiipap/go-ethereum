b7dde6632b39a49c3a4ae284c4b821a0cacd44b0
javiipap <jpiopadilla@gmail.com>
2026-05-27 22:23:41 +0100

    fix: pending block validator changes

--- core/block_validator.go
+++ core/block_validator.go
@@ -172,24 +172,26 @@ func (v *BlockValidator) ValidateState(block *types.Block, statedb *state.StateD
 // to keep the baseline gas close to the provided target, and increase it towards
 // the target if the baseline gas is lower.
 func CalcGasLimit(parentGasLimit, desiredLimit uint64) uint64 {
-	delta := parentGasLimit/params.GasLimitBoundDivisor - 1
-	limit := parentGasLimit
-	if desiredLimit < params.MinGasLimit {
-		desiredLimit = params.MinGasLimit
-	}
-	// If we're outside our allowed gas range, we try to hone towards them
-	if limit < desiredLimit {
-		limit = parentGasLimit + delta
-		if limit > desiredLimit {
-			limit = desiredLimit
-		}
-		return limit
-	}
-	if limit > desiredLimit {
-		limit = parentGasLimit - delta
-		if limit < desiredLimit {
-			limit = desiredLimit
-		}
-	}
-	return limit
+	// delta := parentGasLimit/params.GasLimitBoundDivisor - 1
+	// limit := parentGasLimit
+	// if desiredLimit < params.MinGasLimit {
+	// 	desiredLimit = params.MinGasLimit
+	// }
+	// // If we're outside our allowed gas range, we try to hone towards them
+	// if limit < desiredLimit {
+	// 	limit = parentGasLimit + delta
+	// 	if limit > desiredLimit {
+	// 		limit = desiredLimit
+	// 	}
+	// 	return limit
+	// }
+	// if limit > desiredLimit {
+	// 	limit = parentGasLimit - delta
+	// 	if limit < desiredLimit {
+	// 		limit = desiredLimit
+	// 	}
+	// }
+	// return limit
+
+	return parentGasLimit
 }
